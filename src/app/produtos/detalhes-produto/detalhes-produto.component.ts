58767a744e85da04199e2e882994e01b9afa2303
Moises Alvarenga <moises.alvarenga@outlook.com>
2022-09-04 14:29:35 -0300

    Pagina de carrinho e suas funcionalidades concluidas

--- src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { CarrinhoService } from 'src/app/carrinho.service';
+import { NotificacaoService } from 'src/app/notificacao.service';
+import { IProduto, IProdutoCarrinho } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
   selector: 'app-detalhes-produto',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalhesProdutoComponent implements OnInit {
 
-  constructor() { }
+  produto: IProduto | undefined;
+  quantidade = 1;
+
+  constructor(
+    private produtoService: ProdutosService,
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService,
+    private carrinhoService: CarrinhoService
+  ) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtoService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho(){
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho.");
+    const produto: IProdutoCarrinho = {
+      ...this.produto!,
+      quantidade: this.quantidade
+    }
+    this.carrinhoService.adicionarAoCarrinho(produto);
   }
 
 }
